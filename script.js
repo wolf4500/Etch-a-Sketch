cc58d36c88bcbf2388166a58bdc7b8649ded3b1b
Aaron Dang <aarondang4500@gmail.com>
2024-01-08 16:01:08 -0600

    Simplify grid logic

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
-const container = document.querySelector(".container");
+const GRIDSIZE = 600;
+let rows = 16;
+let cols = 16;
 
-const divs = ["div1", "div2", "div3", "div4", "div5", "div6", "div7", "div8", "div9", "div10",
- "div11", "div12", "div13", "div14", "div15", "div16"];
+// Set width and height of sketch area
+const sketchArea = document.querySelector("#sketch-area");
+sketchArea.style.width = `${GRIDSIZE}px`;
+sketchArea.style.height = `${GRIDSIZE}px`;
 
-const innerDivs = ["innerDiv1", "innerDiv2", "innerDiv3", "innerDiv4", "innerDiv5", "innerDiv6",
- "innerDiv7", "innerDiv8", "innerDiv9", "innerDiv10", "innerDiv11", "innerDiv12", "innerDiv13", 
- "innerDiv14", "innerDiv15", "innerDiv16"]
+// Create Grid
+function createGrid(){
+    for(let i = 0; i < rows * cols; i++) {
+        gridCell = document.createElement("div");
 
- const divClass = ["class1", "class2", "class3", "class4", "class5", "class6", "class7", "class8",
- "class9", "class10", "class11", "class12", "class13", "class14", "class15", "class16"]
+        gridCell.style.width = `${(GRIDSIZE / cols) - 2}px`;
+        gridCell.style.height = `${(GRIDSIZE / rows) - 2}px`;
+        gridCell.classList.add("cell");
 
-for(let i = 0; i <= 15; i++)
-{
-    divs[i] = document.createElement("div"); 
-    container.appendChild(divs[i]);
-
-    for(let n = 0; n <= 15; n++)
-    {
-        innerDivs[n] = document.createElement("div");
-        innerDivs[n].setAttribute('style', 'border-color:black; border-style: solid; width: 20px; height: 20px;')
-        divs[i].appendChild(innerDivs[n]);
-
-        innerDivs[n].addEventListener("mouseover", (event) => {
-            event.target.style.backgroundColor = "grey";
-        })
-
+        sketchArea.appendChild(gridCell);
     }
 }
+
+// Call createGrid function 
+createGrid();
+
