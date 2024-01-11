313934c292dd220b991e4669247533caddd89a49
Aaron Dang <aarondang4500@gmail.com>
2024-01-11 16:54:34 -0600

    Add a slider and a button to confirm new grid values

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const GRIDSIZE = 600;
-let squaresPerSide = 16;
+let sideLength = 16;
 
 // Set width and height of sketch area
 const sketchArea = document.querySelector("#sketch-area");
@@ -11,10 +11,11 @@ function fillGrid() {
 }
 
 // Create Grid
-function createGrid(){
+function createGrid(squaresPerSide){
     const totalSquares = squaresPerSide * squaresPerSide;
     const widthOrHeight = `${(GRIDSIZE / squaresPerSide) - 2}px`;
-    
+
+    // Add grid cells to the sketch area
     for(let i = 0; i < totalSquares; i++) {
         gridCell = document.createElement("div");
 
@@ -29,5 +30,37 @@ function createGrid(){
 }
 
 // Call createGrid function 
-createGrid();
+createGrid(sideLength);
+
+// Changes grid baesd on new slider value
+function changeGrid() {
+
+    // Remove the current grid before placing new one
+    while(sketchArea.firstChild) {
+        sketchArea.removeChild(sketchArea.firstChild);
+    }
+
+    // Prompt user to choose grid size
+    // let newGrid = prompt("Enter a grid size between 1-100 (a X a)");
+    // newGrid = parseInt(newGrid);
+    // console.log(newGrid);
+    // while(newGrid < 0 || newGrid > 100 || newGrid) {
+    //     newGrid = prompt("Enter a valid grid size between 1-100 (a X a)");
+    // }
+    let newGrid = slider.value;
+    // Use user-prompted grid size to create new grid
+    createGrid(newGrid);
+}
+
+const slider = document.querySelector(".slider");
+const rangeOutput = document.querySelector("#range-display");
+slider.addEventListener("input", rangeDisplay);
+
+function rangeDisplay() {
+    rangeOutput.innerHTML = slider.value + " x " + slider.value;
+}
+
+const button = document.querySelector("#apply-button");
+button.addEventListener("click", changeGrid);
+
 
